87eb09af7facbf678e3be1012b8fcc97b8f98843
DevFabrizio <rockout94@hotmail.it>
2023-02-27 11:53:28 +0000

    add JS variables to be defined after content load

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// undefined variables ready to be defined after content loading
+
+let infoIcon = ;
+let mortarInput = ;
+let sandInput = ;
+let cementInput = ;
+let mortarQty = ;
+let sandQty = ;
+let cementQty = ;
+let redBricksInput = ;
+let stoneBricksInput = ;
+let concreteBricksInput = ;
+let redBricksQty = ;
+let stoneBricksQty = ;
+let concreteBricksQty = ;
+let hoursOfWorkInput = ;
+let totalCost = ; 
