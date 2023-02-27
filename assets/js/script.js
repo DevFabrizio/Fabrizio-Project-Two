6bc4b517e61469ce99ce6a238247ca52cb5c9b14
DevFabrizio <rockout94@hotmail.it>
2023-02-27 17:50:50 +0000

    define the JS variables inside a function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,24 @@
-// undefined variables ready to be defined after content loading
+// variables ready to be defined after content loading
 
-let infoIcon = ;
-let mortarInput = ;
-let sandInput = ;
-let cementInput = ;
-let mortarQty = ;
-let sandQty = ;
-let cementQty = ;
-let redBricksInput = ;
-let stoneBricksInput = ;
-let concreteBricksInput = ;
-let redBricksQty = ;
-let stoneBricksQty = ;
-let concreteBricksQty = ;
-let hoursOfWorkInput = ;
-let totalCost = ; 
+document.addEventListener("DOMContentLoaded", function() {
+
+    let infoIcon = document.getElementById("info-icon");
+    let mortarInput = document.getElementById("mortar");
+    let sandInput = document.getElementById("sand");
+    let cementInput = document.getElementById("cement");
+    let mortarQty = document.getElementById("mortar-qty");
+    let sandQty = document.getElementById("sand-qty");
+    let cementQty = document.getElementById("cement-qty");
+    let redBricksInput = document.getElementById("red-bricks");
+    let stoneBricksInput = document.getElementById("stone-bricks");
+    let concreteBricksInput = document.getElementById("concrete-bricks");
+    let redBricksQty = document.getElementById("red-bricks-qty");
+    let stoneBricksQty =document.getElementById("stone-bricks-qty") ;
+    let concreteBricksQty =document.getElementById("concrete-bricks-qty") ;
+    let hoursOfWorkInput = document.getElementById("hours-of-work");
+    let totalCost = document.getElementById("total-cost");
+      
+})
+
+// list of functions to shift through materials, bricks and hours of work
+
