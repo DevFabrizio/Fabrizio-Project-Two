af7e8460ae2891847e2cd330a8cce4f759f4a6fb
DevFabrizio <rockout94@hotmail.it>
2023-03-10 09:07:31 +0000

    add and style initial form for user info

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,9 +30,9 @@ const totalCost = ''
     const redBricksInput = document.getElementById("red-bricks");
     const stoneBricksInput = document.getElementById("stone-bricks");
     const concreteBricksInput = document.getElementById("concrete-bricks");
-    const redBricksQty = document.getElementById("red-bricks-qty");
-    const stoneBricksQty =document.getElementById("stone-bricks-qty") ;
-    const concreteBricksQty =document.getElementById("concrete-bricks-qty") ;
+    const redBricksQty = document.getElementById("red-bricks-quantity");
+    const stoneBricksQty = document.getElementById("stone-bricks-quantity") ;
+    const concreteBricksQty = document.getElementById("concrete-bricks-quantity") ;
     const hoursOfWorkInput = document.getElementById("hours-of-work");
     const totalCost = document.getElementById("total-cost");
       
@@ -58,14 +58,14 @@ for (buttons of nextButton){
 function backToMainPage(event){
 
     document.getElementById('total-cost').style.display = 'none';
-    document.getElementById('materials').style.display = 'flex';
+    document.getElementById('form').style.display = 'flex';
 
 }
 
 let mainPageButton = document.getElementById('back');
 mainPageButton.addEventListener('click', backToMainPage);
 
-// function to show the website instructions
+// function to show the website instructions on all sections
 
 function information(event){
     let bgImage = document.getElementById('interactive-section-bg');
@@ -89,32 +89,45 @@ infoBox.addEventListener('click', information);
 
 
 function displayNumber(event){
+    
+    
     let mortar = document.getElementById('mortar-quantity').value;
     let sand = document.getElementById('sand-quantity').value;
     let cement = document.getElementById('cement-quantity').value;
     let redBricks = document.getElementById('red-bricks-quantity').value;
     let stoneBricks = document.getElementById('stone-bricks-quantity').value;
     let concreteBricks = document.getElementById('concrete-bricks-quantity').value;
+    let hoursOfWork = document.getElementById('total-hours').value;
     let price = document.getElementById('price');
+    let inputArray = [mortar, sand, cement, redBricks, stoneBricks, concreteBricks];  
+          
+            let listItem = `<li>${mortar} kg of mortar</li>
+            <li>${sand} kg of sand</li>
+            <li>${cement} kg of cement</li>
+            <li>${redBricks} kg of red bricks</li>
+            <li>${stoneBricks} kg of stone bricks</li>
+            <li>${concreteBricks} kg of concrete bricks</li>
+            <li>${hoursOfWork} hours of work</li>`;
+            
+            // retrieve the ul element from the DOM
+            
+            let ul = document.getElementsByTagName('ul')[0];
+            
+            // insert the value provided by the user multiplied by their price to generate final price
+            
+            price.textContent = mortar * 0.70 +
+            sand * 0.10 +
+            cement * 0.80 +
+            redBricks * 1 +
+            stoneBricks * 1.50 +
+            concreteBricks * 0.90 + ' euros';
+            
+            ul.innerHTML = listItem;
+            
     
-    let listItem = `<li>${mortar} kg of mortar</li>
-                    <li>${sand} kg of sand</li>
-                    <li>${cement} kg of cement</li>
-                    <li>${redBricks}</li>
-                    <li>${stoneBricks}</li>
-                    <li>${concreteBricks}</li>`;
-    
-    // functions to run the quote generator
-    let ul = document.getElementsByTagName('ul')[0];
-    price.textContent = mortar * 0.70 +
-                        sand * 0.10 +
-                        cement * 0.80 +
-                        redBricks * 1 +
-                        stoneBricks * 1.50 +
-                        concreteBricks * 0.90 + ' euros';
-
-    ul.innerHTML = listItem;
-    
+        
+   
 }
+
 let submitButton = document.getElementsByClassName('submit')[0];
 submitButton.addEventListener('click', displayNumber);
